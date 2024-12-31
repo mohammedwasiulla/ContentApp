64db752f3528af9a15242839f7714495e8907407
mohammed wasiullah <mohammedwasiullah6@gmail.com>
2024-12-31 02:15:00 -0800

    Add redux store file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from "./redux/store"; // Import from your project's store file;
+import { store } from "./redux/Store"; // Import from your project's store file;
 import { Provider } from 'react-redux'
 import {Toaster} from 'react-hot-toast'
 
